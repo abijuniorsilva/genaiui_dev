3bb461e12ebd97e283248e70f434bebef610993e
SILVA Junior (External) <junior.silva@amaris.com>
2023-10-26 11:59:23 -0300

    deploy config

--- src/App.jsx
+++ src/App.jsx
@@ -9,12 +9,12 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Layout />}>
+        <Route path="/genaiui_dev" element={<Layout />}>
           <Route index element={<Dashboard />} />
-          <Route path="products" element={<Products />} />
-          <Route path="home" element={<Home />} />
+          <Route path="genaiui_dev/products" element={<Products />} />
+          <Route path="genaiui_dev/home" element={<Home />} />
         </Route>
-        <Route path="login" element={<Login/>} />
+        <Route path="genaiui_dev/login" element={<Login/>} />
       </Routes>
     </Router>
   );
